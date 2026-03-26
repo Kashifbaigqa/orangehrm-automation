07ba1067f6e4bf0a84c75ad6bc92af10c124146d
kashif baig <kashif.baig.qa@gmail.com>
2026-03-26 21:12:16 +0530

    add BaseClass setup with config-driven browser initialization and TestNG test integration
    - Load config.properties in BaseClass
    - Initialize WebDriver based on browser parameter
    - Apply implicit wait and navigate to URL
    - Extend BaseClass in test class
    - Add TestNG @Test for execution

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -1,5 +1,60 @@
 package com.orangehrm.base;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
 public class BaseClass {
 
+	// making these objects protected so we can use withing the pakage and outside
+	// the pakage by creating
+	protected Properties prop;
+	protected WebDriver driver;
+
+	@BeforeMethod
+	public void setup() throws IOException {
+
+		// Step 1:Load the configruation file
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+		prop.load(fis);
+
+		// Step 2: Initialize the WebDriver based on browser defined in
+		// config.properties file
+
+		String browser = prop.getProperty("browser");
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			throw new IllegalArgumentException("Browser not supported:" + browser);
+		}
+
+		// implicit wait
+		int implicitwait = Integer.parseInt(prop.getProperty("implicitWait"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitwait));
+
+		// Step 3:Navigate to URL
+		driver.get(prop.getProperty("url"));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		if(driver!=null) {
+			driver.quit();
+		}
+	}
 }
--- src/test/java/com/orangehrm/test/LoginPageTest.java
+++ src/test/java/com/orangehrm/test/LoginPageTest.java
@@ -1,5 +1,21 @@
 package com.orangehrm.test;
 
-public class LoginPageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.orangehrm.base.BaseClass;
+
+public class LoginPageTest extends BaseClass {
+
+@Test	
+public void dummyTest() {
+
+String title = driver.getTitle();
+//assert title.equals("OrangeHRM"): "Test Failed - Title is Not Matching"; java asset syntax using testNG syntax instead
+
+Assert.assertEquals(title, "OrangeHRM", "Title mismatch");
+System.out.println("Test Passed - Title is Matching");
 
 }
+}
+
